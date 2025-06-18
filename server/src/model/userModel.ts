61bcad755e574edc8f0e0efdb85996ca7e5f4e58
Arun <arun11.kb@gmail.com>
2025-06-18 15:12:44 +0530

    feat: Add user model indexing

--- server/src/model/userModel.ts
+++ server/src/model/userModel.ts
@@ -17,6 +17,9 @@ const UserSchema = new mongoose.Schema<IUserDb>({
   accessToken: { type: String },
 }, { timestamps: true })
 
+UserSchema.index({ accessToken: 1, email: 1, name: 1, password: 1, _id: 1 })
+UserSchema.index({ email: 1, accessToken: 1, name: 1, password: 1, _id: 1 })
+
 const userModel = mongoose.model('users', UserSchema)
 
 export default userModel
