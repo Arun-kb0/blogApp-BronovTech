c1f258a48782b395136393fd0f5b1de194c7e2f6
Arun <arun11.kb@gmail.com>
2025-06-18 13:42:16 +0530

    feat: Add db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import connectDb from './config/connectDb'
 
 const PORT = process.env.PORT || 3001
 const MONGO_DB_URI = process.env.MONGO_DB_URI || ''
@@ -15,6 +16,10 @@ app.get('/test', (req: Request, res: Response) => {
   res.json({ message: 'test route: server is running' })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+connectDb(MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch(error => console.log(error))
