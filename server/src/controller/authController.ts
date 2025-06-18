0a43d2e09521d796f09f6574978fac2b5113c9de
Arun <arun11.kb@gmail.com>
2025-06-18 15:11:39 +0530

    feat: Add login & logout endpoints

--- server/src/controller/authController.ts
+++ server/src/controller/authController.ts
@@ -3,7 +3,7 @@ import HttpError from "../util/HttpError";
 import httpStatus from "../constants/httpStatus";
 import userModel from "../model/userModel";
 import bcrypt from 'bcrypt'
-import jwt, { SignOptions, Secret } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'secret'
@@ -44,17 +44,49 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
   }
 }
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
+export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    
+    const { email, password } = req.body
+    console.log(email, password)
+    if (typeof email !== 'string' || typeof password !== 'string') {
+      throw new HttpError(httpStatus.BAD_REQUEST, 'name,email and password are required')
+    }
+    const foundUser = await userModel.findOne({ email })
+    if (!foundUser) throw new HttpError(httpStatus.NOT_FOUND, 'User not found.')
+
+    const isMatch = await bcrypt.compare(password, foundUser.password)
+    if (!isMatch) throw new HttpError(httpStatus.UNAUTHORIZED, 'Password miss match.')
+    const accessToken = jwt.sign(
+      { "email": email, "userId": foundUser._id },
+      ACCESS_TOKEN_SECRET,
+      { expiresIn: '1h' }
+    )
+    const updatedUser = await userModel.findOneAndUpdate(
+      { _id: foundUser._id },
+      { accessToken: accessToken },
+      { new: true, projection: { password: 0 } }
+    )
+    if (!updatedUser) throw new Error('Signup failed')
+
+    res.status(httpStatus.OK).json({ user: updatedUser })
   } catch (error) {
     next(error)
   }
 }
 
-export const logout = (req: Request, res: Response, next: NextFunction) => {
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { accessToken } = req.query
+    if (typeof accessToken !== 'string') throw new HttpError(httpStatus.NOT_FOUND, 'Logout success.')
 
+    const foundUser = await userModel.findOne({ accessToken })
+    if (!foundUser) throw new HttpError(httpStatus.NOT_FOUND, 'Logout success.')
+    const updatedUser = await userModel.findOneAndUpdate(
+      { _id: foundUser._id },
+      { accessToken: null },
+      { new: true, projection: { password: 0 } }
+    )
+    res.status(httpStatus.OK).json({ status: 'success' })
   } catch (error) {
     next(error)
   }
